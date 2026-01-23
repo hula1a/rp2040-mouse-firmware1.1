6c44030238a6ade6de1a6eeb0bd741d68308b1cb
hula1a <1770709854@qq.com>
2026-01-23 17:55:07 +0800

    Update tusb_config.h

--- tusb_config.h
+++ tusb_config.h
@@ -1,29 +1,20 @@
 #ifndef _TUSB_CONFIG_H_
 #define _TUSB_CONFIG_H_
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-// Board config
 #define CFG_TUSB_RHPORT0_MODE   OPT_MODE_DEVICE
 #define CFG_TUSB_OS             OPT_OS_NONE
 
-// 禁用 CDC
+// 禁用所有不需要的
 #define CFG_TUD_CDC             0
-
-// 禁用 MSC
 #define CFG_TUD_MSC             0
+#define CFG_TUD_VENDOR          0
+#define CFG_TUD_MIDI            0
+#define CFG_TUD_AUDIO           0
 
-// 启用 HID
+// 只启用 HID
 #define CFG_TUD_HID             1
 
-// 端点配置
 #define CFG_TUD_ENDPOINT0_SIZE  64
-#define CFG_TUD_HID_EP_BUFSIZE  64
-
-#ifdef __cplusplus
-}
-#endif
+#define CFG_TUD_HID_EP_BUFSIZE  16
 
 #endif
