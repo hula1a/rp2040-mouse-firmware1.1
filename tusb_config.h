e6d590a25dfa6de89e57b882553c43c50453e778
hula1a <1770709854@qq.com>
2026-01-24 14:27:41 +0800

    Update tusb_config.h

--- tusb_config.h
+++ tusb_config.h
@@ -5,17 +5,31 @@
 extern "C" {
 #endif
 
-#define CFG_TUSB_RHPORT0_MODE   OPT_MODE_DEVICE
-#define CFG_TUSB_OS             OPT_OS_NONE
+//--------------------------------------------------------------------
+// 通用配置
+//--------------------------------------------------------------------
+#define CFG_TUSB_MCU                OPT_MCU_RP2040
+#define CFG_TUSB_RHPORT0_MODE       OPT_MODE_DEVICE
+#define CFG_TUSB_OS                 OPT_OS_NONE
+#define CFG_TUSB_MEM_SECTION
+#define CFG_TUSB_MEM_ALIGN          __attribute__ ((aligned(4)))
 
-#define CFG_TUD_CDC             0
-#define CFG_TUD_MSC             0
-#define CFG_TUD_HID             1
-#define CFG_TUD_MIDI            0
-#define CFG_TUD_VENDOR          0
+//--------------------------------------------------------------------
+// 设备配置
+//--------------------------------------------------------------------
+#define CFG_TUD_ENDPOINT0_SIZE      64
 
-#define CFG_TUD_ENDPOINT0_SIZE  64
-#define CFG_TUD_HID_EP_BUFSIZE  16
+// 只启用 HID，禁用其他所有类
+#define CFG_TUD_HID                 1
+#define CFG_TUD_CDC                 0
+#define CFG_TUD_MSC                 0
+#define CFG_TUD_MIDI                0
+#define CFG_TUD_VENDOR              0
+
+//--------------------------------------------------------------------
+// HID 配置
+//--------------------------------------------------------------------
+#define CFG_TUD_HID_EP_BUFSIZE      8
 
 #ifdef __cplusplus
 }
