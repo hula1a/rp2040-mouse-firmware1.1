1b150d8a1b6bb81028150bb9482c23bdb15ff987
hula1a <1770709854@qq.com>
2026-01-24 14:15:03 +0800

    Update tusb_config.h

--- tusb_config.h
+++ tusb_config.h
@@ -5,20 +5,15 @@
 extern "C" {
 #endif
 
-// 设备模式
 #define CFG_TUSB_RHPORT0_MODE   OPT_MODE_DEVICE
 #define CFG_TUSB_OS             OPT_OS_NONE
 
-// 禁用不需要的功能
 #define CFG_TUD_CDC             0
 #define CFG_TUD_MSC             0
+#define CFG_TUD_HID             1
 #define CFG_TUD_MIDI            0
 #define CFG_TUD_VENDOR          0
 
-// 启用 HID
-#define CFG_TUD_HID             1
-
-// 缓冲区配置
 #define CFG_TUD_ENDPOINT0_SIZE  64
 #define CFG_TUD_HID_EP_BUFSIZE  16
 
