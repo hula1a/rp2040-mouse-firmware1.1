273884df9934824314471b727ce6f92279d3047f
hula1a <1770709854@qq.com>
2026-01-24 13:55:38 +0800

    Update tusb_config.h

--- tusb_config.h
+++ tusb_config.h
@@ -5,17 +5,49 @@
 extern "C" {
 #endif
 
-#define CFG_TUSB_RHPORT0_MODE   OPT_MODE_DEVICE
-#define CFG_TUSB_OS             OPT_OS_NONE
+//--------------------------------------------------------------------
+// COMMON CONFIGURATION
+//--------------------------------------------------------------------
 
-#define CFG_TUD_CDC             0
-#define CFG_TUD_MSC             0
-#define CFG_TUD_HID             1
-#define CFG_TUD_MIDI            0
-#define CFG_TUD_VENDOR          0
+// defined by board.mk
+#ifndef CFG_TUSB_MCU
+  #error CFG_TUSB_MCU must be defined
+#endif
 
-#define CFG_TUD_ENDPOINT0_SIZE  64
-#define CFG_TUD_HID_EP_BUFSIZE  16
+#ifndef CFG_TUSB_OS
+#define CFG_TUSB_OS           OPT_OS_NONE
+#endif
+
+#ifndef CFG_TUSB_DEBUG
+#define CFG_TUSB_DEBUG        0
+#endif
+
+// Enable Device stack
+#define CFG_TUD_ENABLED       1
+
+// RHPort number used for device
+#define BOARD_TUD_RHPORT      0
+
+// RHPort max operational speed
+#define BOARD_TUD_MAX_SPEED   OPT_MODE_FULL_SPEED
+
+//--------------------------------------------------------------------
+// DEVICE CONFIGURATION
+//--------------------------------------------------------------------
+
+#ifndef CFG_TUD_ENDPOINT0_SIZE
+#define CFG_TUD_ENDPOINT0_SIZE    64
+#endif
+
+//------------- CLASS -------------//
+#define CFG_TUD_CDC               0
+#define CFG_TUD_MSC               0
+#define CFG_TUD_HID               1
+#define CFG_TUD_MIDI              0
+#define CFG_TUD_VENDOR            0
+
+// HID buffer size
+#define CFG_TUD_HID_EP_BUFSIZE    16
 
 #ifdef __cplusplus
 }
