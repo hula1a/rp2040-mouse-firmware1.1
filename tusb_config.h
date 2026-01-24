a28da56d4a814e5c6909dbe59bc668cf6438f1c8
hula1a <1770709854@qq.com>
2026-01-24 17:02:13 +0800

    Update tusb_config.h

--- tusb_config.h
+++ tusb_config.h
@@ -1,38 +1,24 @@
-#ifndef _TUSB_CONFIG_H_
-#define _TUSB_CONFIG_H_
+#ifndef TUSB_CONFIG_H
+#define TUSB_CONFIG_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-//--------------------------------------------------------------------
-// 通用配置
-//--------------------------------------------------------------------
-#define CFG_TUSB_MCU                OPT_MCU_RP2040
+// USB 模式：设备模式
 #define CFG_TUSB_RHPORT0_MODE       OPT_MODE_DEVICE
-#define CFG_TUSB_OS                 OPT_OS_NONE
-#define CFG_TUSB_MEM_SECTION
-#define CFG_TUSB_MEM_ALIGN          __attribute__ ((aligned(4)))
 
-//--------------------------------------------------------------------
-// 设备配置
-//--------------------------------------------------------------------
-#define CFG_TUD_ENDPOINT0_SIZE      64
-
-// 只启用 HID，禁用其他所有类
-#define CFG_TUD_HID                 1
+// 禁用 CDC（关键！）
 #define CFG_TUD_CDC                 0
+
+// 只启用 HID
+#define CFG_TUD_HID                 1
 #define CFG_TUD_MSC                 0
 #define CFG_TUD_MIDI                0
 #define CFG_TUD_VENDOR              0
 
-//--------------------------------------------------------------------
 // HID 配置
-//--------------------------------------------------------------------
 #define CFG_TUD_HID_EP_BUFSIZE      8
+#define CFG_TUD_ENDPOINT0_SIZE      64
 
-#ifdef __cplusplus
-}
-#endif
+// 内存配置（关键！之前漏了）
+#define CFG_TUSB_MEM_SECTION        __attribute__((section(".usb_ram")))
+#define CFG_TUSB_MEM_ALIGN          __attribute__((aligned(4)))
 
 #endif
