926f6e08253f2761117b5487952b9845e5edd2cc
hula1a <1770709854@qq.com>
2026-01-30 15:40:33 +0800

    Update usb_descriptors.c

--- usb_descriptors.c
+++ usb_descriptors.c
@@ -1,133 +1,30 @@
-#include "tusb.h"
-#include <string.h>
+cmake_minimum_required(VERSION 3.13)
 
-#define ZOWIE_VID   0x04A5
-#define ZOWIE_PID   0x8002
+include($ENV{PICO_SDK_PATH}/external/pico_sdk_import.cmake)
 
-// 设备描述符
-tusb_desc_device_t const desc_device = {
-    .bLength            = sizeof(tusb_desc_device_t),
-    .bDescriptorType    = TUSB_DESC_DEVICE,
-    .bcdUSB             = 0x0200,
-    .bDeviceClass       = 0x00,
-    .bDeviceSubClass    = 0x00,
-    .bDeviceProtocol    = 0x00,
-    .bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
-    .idVendor           = ZOWIE_VID,
-    .idProduct          = ZOWIE_PID,
-    .bcdDevice          = 0x0100,
-    .iManufacturer      = 1,
-    .iProduct           = 2,
-    .iSerialNumber      = 3,
-    .bNumConfigurations = 1
-};
+project(zowie_mouse C CXX ASM)
+set(CMAKE_C_STANDARD 11)
+set(CMAKE_CXX_STANDARD 17)
 
-uint8_t const* tud_descriptor_device_cb(void) {
-    return (uint8_t const*)&desc_device;
-}
+pico_sdk_init()
 
-// HID 报告描述符
-uint8_t const desc_hid_report[] = {
-    0x05, 0x01,        // Usage Page (Generic Desktop)
-    0x09, 0x02,        // Usage (Mouse)
-    0xA1, 0x01,        // Collection (Application)
-    0x09, 0x01,        //   Usage (Pointer)
-    0xA1, 0x00,        //   Collection (Physical)
+add_executable(zowie_mouse
+    main.c
+    usb_descriptors.c
+)
 
-    // Buttons (5个)
-    0x05, 0x09,        //     Usage Page (Button)
-    0x19, 0x01,        //     Usage Minimum (1)
-    0x29, 0x05,        //     Usage Maximum (5)
-    0x15, 0x00,        //     Logical Minimum (0)
-    0x25, 0x01,        //     Logical Maximum (1)
-    0x95, 0x05,        //     Report Count (5)
-    0x75, 0x01,        //     Report Size (1)
-    0x81, 0x02,        //     Input (Data,Var,Abs)
+# 禁用所有 stdio（不要 CDC）
+pico_enable_stdio_usb(zowie_mouse 0)
+pico_enable_stdio_uart(zowie_mouse 0)
 
-    // Padding (3 bits)
-    0x95, 0x01,        //     Report Count (1)
-    0x75, 0x03,        //     Report Size (3)
-    0x81, 0x03,        //     Input (Const)
+target_include_directories(zowie_mouse PRIVATE ${CMAKE_CURRENT_LIST_DIR})
 
-    // X, Y
-    0x05, 0x01,        //     Usage Page (Generic Desktop)
-    0x09, 0x30,        //     Usage (X)
-    0x09, 0x31,        //     Usage (Y)
-    0x15, 0x81,        //     Logical Minimum (-127)
-    0x25, 0x7F,        //     Logical Maximum (127)
-    0x75, 0x08,        //     Report Size (8)
-    0x95, 0x02,        //     Report Count (2)
-    0x81, 0x06,        //     Input (Data,Var,Rel)
+target_link_libraries(zowie_mouse
+    pico_stdlib
+    hardware_uart
+    hardware_irq
+    tinyusb_device
+    tinyusb_board
+)
 
-    // Wheel
-    0x09, 0x38,        //     Usage (Wheel)
-    0x15, 0x81,        //     Logical Minimum (-127)
-    0x25, 0x7F,        //     Logical Maximum (127)
-    0x75, 0x08,        //     Report Size (8)
-    0x95, 0x01,        //     Report Count (1)
-    0x81, 0x06,        //     Input (Data,Var,Rel)
-
-    0xC0,              //   End Collection
-    0xC0               // End Collection
-};
-
-uint8_t const* tud_hid_descriptor_report_cb(uint8_t instance) {
-    (void)instance;
-    return desc_hid_report;
-}
-
-// 配置描述符
-enum { ITF_NUM_HID, ITF_NUM_TOTAL };
-#define CONFIG_TOTAL_LEN  (TUD_CONFIG_DESC_LEN + TUD_HID_DESC_LEN)
-#define EPNUM_HID         0x81
-
-uint8_t const desc_configuration[] = {
-    TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, 0x00, 100),
-    TUD_HID_DESCRIPTOR(ITF_NUM_HID, 0, HID_ITF_PROTOCOL_MOUSE, sizeof(desc_hid_report), EPNUM_HID, CFG_TUD_HID_EP_BUFSIZE, 1)
-};
-
-uint8_t const* tud_descriptor_configuration_cb(uint8_t index) {
-    (void)index;
-    return desc_configuration;
-}
-
-// 字符串描述符
-char const* string_desc_arr[] = {
-    (const char[]){0x09, 0x04},
-    "Zowie Gear",
-    "Zowie Gaming Mouse",
-    "GBA5H0045004A",
-};
-
-static uint16_t _desc_str[32];
-
-uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
-    (void)langid;
-    uint8_t chr_count;
-
-    if (index == 0) {
-        memcpy(&_desc_str[1], string_desc_arr[0], 2);
-        chr_count = 1;
-    } else {
-        if (index >= sizeof(string_desc_arr)/sizeof(string_desc_arr[0])) return NULL;
-        const char* str = string_desc_arr[index];
-        chr_count = strlen(str);
-        if (chr_count > 31) chr_count = 31;
-        for (uint8_t i = 0; i < chr_count; i++) {
-            _desc_str[1 + i] = str[i];
-        }
-    }
-
-    _desc_str[0] = (TUSB_DESC_STRING << 8) | (2 * chr_count + 2);
-    return _desc_str;
-}
-
-// HID 回调
-uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen) {
-    (void)instance; (void)report_id; (void)report_type; (void)buffer; (void)reqlen;
-    return 0;
-}
-
-void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize) {
-    (void)instance; (void)report_id; (void)report_type; (void)buffer; (void)bufsize;
-}
+pico_add_extra_outputs(zowie_mouse)
