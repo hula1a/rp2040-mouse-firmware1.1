76c963f883699fc987eefa00b5a48872e0f9c871
hula1a <1770709854@qq.com>
2026-01-30 16:21:14 +0800

    Update tusb_config.h

--- tusb_config.h
+++ tusb_config.h
@@ -2,7 +2,6 @@
 #define _TUSB_CONFIG_H_
 
 #define CFG_TUSB_RHPORT0_MODE   OPT_MODE_DEVICE
-#define CFG_TUSB_OS             OPT_OS_NONE
 
 // 只启用 HID
 #define CFG_TUD_HID             1
@@ -12,8 +11,5 @@
 #define CFG_TUD_VENDOR          0
 
 #define CFG_TUD_HID_EP_BUFSIZE  8
-
-#define CFG_TUSB_MEM_SECTION
-#define CFG_TUSB_MEM_ALIGN      __attribute__((aligned(4)))
 
 #endif
